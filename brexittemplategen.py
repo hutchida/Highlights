f2f725bee63b33d9c3fd7c6f34e9f141dde15131
Daniel Hutchings <39595766+hutchida@users.noreply.github.com>
2019-08-21 11:01:21 +0100

    Ready for live

--- brexittemplategen.py
+++ brexittemplategen.py
@@ -174,29 +174,39 @@ def TemplateGeneration(PA, highlightdate, highlightType, outputDir, NSMAP):
             trsecmain.append(comment) 
         if PA != 'Public Law':
             if PABrexitSection != None: 
-                trsecsub1 = etree.SubElement(trsecmain, '{%s}secsub1' % NSMAP['tr'])
-                coretitle = etree.SubElement(trsecsub1, '{%s}title' % NSMAP['core'])
-                coretitle.text = PA
-                
-                print('Brexit section found in ' + PA)
-                #print(PABrexitSection)
-                PASecSub1s = PABrexitSection.findall('.//tr:secsub1', namespaces=NSMAP)            
-                for PASecSub1 in PASecSub1s:
-                    PASecSub1Title = PASecSub1.find('{%s}title' % NSMAP['core'])
-                    print(PASecSub1Title.text)
-                    PASecSub1Para = PASecSub1.find('{%s}para' % NSMAP['core'])
+                PASecSub1 = PABrexitSection.find('.//tr:secsub1', namespaces=NSMAP)   #find the trsecsub1
+                PASecSub1Title = PASecSub1.find('{%s}title' % NSMAP['core']) #find within it the title
+                if PASecSub1Title.text != None: #check that there is text in the title tags, else skip
+                    trsecsub1 = etree.SubElement(trsecmain, '{%s}secsub1' % NSMAP['tr'])
+                    coretitle = etree.SubElement(trsecsub1, '{%s}title' % NSMAP['core'])
+                    coretitle.text = PA
+                    
+                    print('Brexit section found in ' + PA)
+                    #print(PABrexitSection)
+                    PASecSub1s = PABrexitSection.findall('.//tr:secsub1', namespaces=NSMAP)            
+                    for PASecSub1 in PASecSub1s:
+                        PASecSub1Title = PASecSub1.find('{%s}title' % NSMAP['core'])
+                        print(PASecSub1Title.text)
+                        PASecSub1Para = PASecSub1.find('{%s}para' % NSMAP['core'])
+                        corepara = etree.SubElement(trsecsub1, '{%s}para' % NSMAP['core'])
+                        coreparabold = etree.SubElement(corepara, '{%s}emph' % NSMAP['core'])                    
+                        coreparabold.set('typestyle', 'bf')
+                        coreparabold.text = PASecSub1Title.text                   
+                        try:
+                            trsecsub1.append(PASecSub1Para) 
+                        except:
+                            corepara = etree.SubElement(trsecsub1, '{%s}para' % NSMAP['core'])
+                            
+                            #corepara.text = PASecSub1Para.text
                     corepara = etree.SubElement(trsecsub1, '{%s}para' % NSMAP['core'])
-                    coreparabold = etree.SubElement(corepara, '{%s}emph' % NSMAP['core'])                    
-                    coreparabold.set('typestyle', 'bf')
-                    coreparabold.text = PASecSub1Title.text                   
-                    corepara = etree.SubElement(trsecsub1, '{%s}para' % NSMAP['core'])
-                    corepara.text = PASecSub1Para.text
-                corepara = etree.SubElement(trsecsub1, '{%s}para' % NSMAP['core'])
-                corepara.text = 'For further updates from ' + PA + ', see: '
-                lncicite = etree.SubElement(corepara, '{%s}cite' % NSMAP['lnci'])
-                lncicite.set('normcite', HighlightsOverviewDict.get(PA))
-                lncicontent = etree.SubElement(lncicite, '{%s}content' % NSMAP['lnci'])
-                lncicontent.text = PA + ' weekly highlights—Overview'
+                    corepara.text = 'For further updates from ' + PA + ', see: '
+                    lncicite = etree.SubElement(corepara, '{%s}cite' % NSMAP['lnci'])
+                    lncicite.set('normcite', HighlightsOverviewDict.get(PA))
+                    lncicontent = etree.SubElement(lncicite, '{%s}content' % NSMAP['lnci'])
+                    lncicontent.text = PA + ' weekly highlights—Overview'
+                else:
+                    comment = etree.Comment(PA + ": no Brexit news found")
+                    trsecmain.append(comment) 
             else:
                 comment = etree.Comment(PA + ": no Brexit news found")
                 trsecmain.append(comment) 
@@ -237,57 +247,150 @@ def TemplateGeneration(PA, highlightdate, highlightType, outputDir, NSMAP):
         corelist.append(BrexitLink)
 
     #Brexit new or updated docs, i.e. Links section
-    corepara = etree.SubElement(trsecmain, '{%s}para' % NSMAP['core'])
-    corepara.text = 'The following new Practice Notes on Brexit have also been recommended by LexisPSL lawyers:'
-
     weeklyNewReportFilepath = FindMostRecentFile(reportDir, '*AICER*_UKPSL_weekly_HL_new_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].csv')
     print('Latest weeklyNewReportFilepath:' + weeklyNewReportFilepath)
+    weeklyUpdateReportFilepath = FindMostRecentFile(reportDir, '*AICER*_UKPSL_weekly_HL_updated_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].csv')
+    print('Latest weeklyUpdateReportFilepath:' + weeklyUpdateReportFilepath)
     dfNewHighlights = DFCleanup(pd.read_csv(weeklyNewReportFilepath), ['SubtopicShortcut', 'Shortcut', 'SubtopicShortcutOfShortcut'], reportDir + 'new')
+    dfUpdateHighlights = DFCleanup(pd.read_csv(weeklyUpdateReportFilepath), ['SubtopicShortcut', 'Shortcut', 'SubtopicShortcutOfShortcut'], reportDir + 'updated')
+    dfdpsi = pd.read_csv(lookupdpsi, encoding='utf-8')        
+
+
+    #Updated and New content
+    trsecmain = etree.SubElement(khbody, '{%s}secmain' % NSMAP['tr'])
+    coretitle = etree.SubElement(trsecmain, '{%s}title' % NSMAP['core'])
+    coretitle.text = 'New and updated Brexit related content'
     
-    dfNew = dfNewHighlights[dfNewHighlights.ContentItemType != 'QandAs'] 
-    dfNew = dfNew[dfNew["DocTitle"].str.contains("Brexit", case=False)]
-    dfNew = dfNew.sort_values(['DocTitle'], ascending = True)
-    newHighlightCount = len(dfNew)
-    print(newHighlightCount, 'new Brexit related docs')     
-    if newHighlightCount > 0:     
-        dfdpsi = pd.read_csv(lookupdpsi, encoding='utf-8')        
-        for x in range(0,newHighlightCount):               
-            DocID = dfNew.DocID.iloc[x]     
-            DocTitle = dfNew.DocTitle.iloc[x]
-            ContentType = dfNew.ContentItemType.iloc[x]
-            OriginalPA = dfNew.OriginalContentItemPA.iloc[x]
-            UnderReview = dfNew.UnderReview.iloc[x]
-            dpsi = dfdpsi.loc[(dfdpsi['ContentType'] == ContentType) & (dfdpsi['PA'] == OriginalPA), 'DPSI'].item() #filters dataframe by contenttype and PA then tries to extract the only value under the column of DPSI
-            #pguidlookup = pguidlistDir + dpsi + '.xml'
-            try:
-                pguidlookupdom = etree.parse(pguidlistDir + dpsi + '.xml')
-                #print(DocID, DocTitle, ContentType, dpsi, pguidlookup)
-                tag = pguidlookupdom.find(".//document[@database-id='" + str(DocID) + "']")
-                try: pguid = tag.get('pguid')    
-                except AttributeError: 
-                    pguid = 'notfound'
-                    print('Not found on pguid look up list: ' + str(DocID) + str(DocTitle))         
-                #print(DocID, DocTitle, ContentType, dpsi, pguidlookup, pguid)              
-            except: pguid = 'notfound'
-
-            dpsidocID = str(DocID)
-            doctitle = str(DocTitle)
+    #New
+    if len(dfNewHighlights[(dfNewHighlights.ContentItemType != 'QandAs') & dfNewHighlights.DocTitle.str.contains("Brexit", case=False)]) > 0: #if there are any new docs for the PA, create new doc section
+        
+        ContentTypeList=['PracticeNote', 'Precedent', 'Checklist']
+        for ContentType in ContentTypeList:      
+            dfNew = dfNewHighlights[dfNewHighlights.ContentItemType != 'QandAs'] 
+            dfNew = dfNew[dfNew["DocTitle"].str.contains("Brexit", case=False)]        
+            dfNew = dfNew[dfNew.ContentItemType == ContentType]
+            #print(len(dfNew), ContentType)
             
-            corepara = etree.SubElement(trsecmain, '{%s}para' % NSMAP['core'])
-            lncicite = etree.SubElement(corepara, '{%s}cite' % NSMAP['lnci'])
-            lncicite.set('normcite', pguid)
-            lncicontent = etree.SubElement(lncicite, '{%s}content' % NSMAP['lnci'])
-            lncicontent.text = doctitle
-            if UnderReview == True:
-                comment = etree.Comment('Doc ID: ' + str(DocID) + ' UNDER REVIEW: ' + str(UnderReview))
-                corepara.append(comment) #add comment after link
-            trsecmain.append(corepara)
-    else:
-        comment = etree.Comment("No Brexit related doc found")
-        trsecmain.append(comment) 
+            if len(dfNew) > 0: 
+                dfNew = dfNew.sort_values(['DocTitle'], ascending = True)
+                newHighlightCount = len(dfNew)
+                print(ContentType, newHighlightCount, 'new docs')   
+                if ContentType == 'Precedent':
+                    if newHighlightCount > 1: ContentTypeHeader = 'New Precedents'
+                    else: ContentTypeHeader = 'New Precedent'
 
+                if ContentType == 'PracticeNote':
+                    if newHighlightCount > 1: ContentTypeHeader = 'New Practice Notes'
+                    else: ContentTypeHeader = 'New Practice Note'
 
+                if ContentType == 'Checklist':
+                    if newHighlightCount > 1: ContentTypeHeader = 'New Checklists'
+                    else: ContentTypeHeader = 'New Checklist'
 
+                if newHighlightCount > 0:    
+                    trsecsub1 = etree.SubElement(trsecmain, '{%s}secsub1' % NSMAP['tr'])
+                    coretitle = etree.SubElement(trsecsub1, '{%s}title' % NSMAP['core'])
+                    coretitle.text = ContentTypeHeader   
+                    for x in range(0,newHighlightCount):   
+                        
+                        DocID = dfNew.DocID.iloc[x]     
+                        DocTitle = dfNew.DocTitle.iloc[x]
+                        ContentType = dfNew.ContentItemType.iloc[x]
+                        OriginalPA = dfNew.OriginalContentItemPA.iloc[x]
+                        UnderReview = dfNew.UnderReview.iloc[x]
+
+                        if ContentType == 'Checklist': ContentType = 'PracticeNote'
+                        #print(ContentType, OriginalPA)
+                        dpsi = dfdpsi.loc[(dfdpsi['ContentType'] == ContentType) & (dfdpsi['PA'] == OriginalPA), 'DPSI'].item() #filters dataframe by contenttype and PA then tries to extract the only value under the column of DPSI
+                        #print (dpsi)
+                        #pguidlookup = pguidlistDir + dpsi + '.xml'
+                        try:
+                            pguidlookupdom = etree.parse(pguidlistDir + dpsi + '.xml')
+                            #print(DocID, DocTitle, ContentType, dpsi, pguidlookup)
+                            tag = pguidlookupdom.find(".//document[@database-id='" + str(DocID) + "']")
+                            pguid = tag.get('pguid')
+                        except:
+                            pguid = 'notfound'
+                        
+                        #print(DocID, DocTitle, ContentType, dpsi, pguidlookup, pguid)
+                        
+                        dpsidocID = str(DocID)
+                        doctitle = str(DocTitle)
+                        
+                        corepara = etree.SubElement(trsecsub1, '{%s}para' % NSMAP['core'])
+                        lncicite = etree.SubElement(corepara, '{%s}cite' % NSMAP['lnci'])
+                        lncicite.set('normcite', pguid)
+                        lncicontent = etree.SubElement(lncicite, '{%s}content' % NSMAP['lnci'])
+                        lncicontent.text = doctitle
+                        if UnderReview == True:
+                            comment = etree.Comment('Doc ID: ' + str(DocID) + ' UNDER REVIEW: ' + str(UnderReview))
+                            corepara.append(comment) #add comment after link
+
+    #Updated  
+    if len(dfUpdateHighlights[(dfUpdateHighlights.ContentItemType != 'QandAs') & dfUpdateHighlights.DocTitle.str.contains("Brexit", case=False)]) > 0: #if there are any new docs for the PA, create new doc section 
+        
+        
+        ContentTypeList=['PracticeNote', 'Precedent', 'Checklist']
+        for ContentType in ContentTypeList:    
+            dfUpdate = dfUpdateHighlights[dfUpdateHighlights.ContentItemType != 'QandAs'] 
+            dfUpdate = dfUpdate[dfUpdate["DocTitle"].str.contains("Brexit", case=False)]
+            dfUpdate = dfUpdate[dfUpdate.ContentItemType == ContentType]
+            #print(len(dfNew), ContentType)
+            if len(dfUpdate) > 0: 
+                dfUpdate = dfUpdate.sort_values(['DocTitle'], ascending = True)
+                updateHighlightCount = len(dfUpdate)
+                #print(ContentType, updateHighlightCount, 'updates') 
+                if ContentType == 'Precedent': 
+                    if updateHighlightCount > 1: ContentTypeHeader = 'Updated Precedents'
+                    else: ContentTypeHeader = 'Updated Precedent'
+                if ContentType == 'PracticeNote':
+                    if updateHighlightCount > 1: ContentTypeHeader = 'Updated Practice Notes'
+                    else: ContentTypeHeader = 'Updated Practice Note'
+                if ContentType == 'Checklist':
+                    if updateHighlightCount > 1: ContentTypeHeader = 'Updated Checklists'
+                    else: ContentTypeHeader = 'Updated Checklist'
+
+                if updateHighlightCount > 0:    
+                    trsecsub1 = etree.SubElement(trsecmain, '{%s}secsub1' % NSMAP['tr'])
+                    coretitle = etree.SubElement(trsecsub1, '{%s}title' % NSMAP['core'])
+                    coretitle.text = ContentTypeHeader   
+
+                    for x in range(0,updateHighlightCount):   
+                        DocID = dfUpdate.DocID.iloc[x]     
+                        DocTitle = dfUpdate.DocTitle.iloc[x]
+                        ContentType = dfUpdate.ContentItemType.iloc[x]
+                        OriginalPA = dfUpdate.OriginalContentItemPA.iloc[x]
+                        UnderReview = dfUpdate.UnderReview.iloc[x]
+                        
+                        if ContentType == 'Checklist': ContentType = 'PracticeNote'
+
+                        dpsi = dfdpsi.loc[(dfdpsi['ContentType'] == ContentType) & (dfdpsi['PA'] == OriginalPA), 'DPSI'].item() #filters dataframe by contenttype and PA then tries to extract the only value under the column of DPSI
+                        
+                        
+                        #pguidlookup = pguidlistDir + dpsi + '.xml'
+                        try:
+                            pguidlookupdom = etree.parse(pguidlistDir + dpsi + '.xml')
+                            #print(DocID, DocTitle, ContentType, dpsi, pguidlookup)
+                            #print(DocID, PA, ContentType, dpsi, pguidlookup)
+                            tag = pguidlookupdom.find(".//document[@database-id='" + str(DocID) + "']")
+                            pguid = tag.get('pguid')
+                        except:
+                            pguid = 'notfound'
+                        #print(DocID, DocTitle, ContentType, dpsi, pguidlookup, pguid)
+                        
+                        dpsidocID = str(DocID)
+                        doctitle = str(DocTitle)
+                        
+                        corepara = etree.SubElement(trsecsub1, '{%s}para' % NSMAP['core'])
+                        lncicite = etree.SubElement(corepara, '{%s}cite' % NSMAP['lnci'])
+                        lncicite.set('normcite', pguid)
+                        lncicontent = etree.SubElement(lncicite, '{%s}content' % NSMAP['lnci'])
+                        lncicontent.text = doctitle
+                        if UnderReview == True:
+                            comment = etree.Comment('Doc ID: ' + str(DocID) + ' UNDER REVIEW: ' + str(UnderReview))
+                            corepara.append(comment) #add comment after link
+    
+    
 
     #Brexit QAs
     weeklyNewReportFilepath = FindMostRecentFile(reportDir, '*AICER*_UKPSL_weekly_HL_new_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].csv')
@@ -347,8 +450,8 @@ def TemplateGeneration(PA, highlightdate, highlightType, outputDir, NSMAP):
 
 
     tree = etree.ElementTree(khdoc)
-    #xmlfilepath = outputDir + constantPA + '\\' + constantPA + ' Weekly highlights template ' + highlightDate + ' test.xml'
-    xmlfilepath = localDir + constantPA + '\\' + constantPA + ' Weekly highlights template ' + highlightDate + ' test.xml'
+    xmlfilepath = outputDir + constantPA + '\\' + constantPA + ' Weekly highlights template ' + highlightDate + ' test.xml'
+    #xmlfilepath = localDir + constantPA + '\\' + constantPA + ' Weekly highlights template ' + highlightDate + ' test.xml'
     tree.write(xmlfilepath,encoding='utf-8')
 
     f = open(xmlfilepath,'r', encoding='utf-8')
@@ -405,9 +508,10 @@ def DFCleanup(df, ShortcutTypeList, ReportType):
         DocTitle = df.DocTitle.iloc[i]
         UnderReview = df.UnderReview.iloc[i]
         if ShortcutTypeList.count(ContentItemType) > 0: #if found in list
-            ContentItemType = df.OriginalContentItemType.iloc[i]
-            DocID = df.OriginalContentItemId.iloc[i]
-            OriginalContentItemPA = df.OriginalContentItemPA.iloc[i]
+            continue #filter out shortcuts
+            #ContentItemType = df.OriginalContentItemType.iloc[i]
+            #DocID = df.OriginalContentItemId.iloc[i]
+            #OriginalContentItemPA = df.OriginalContentItemPA.iloc[i]
 
         list1 = [[DocID, ContentItemType, PA, OriginalContentItemPA, DocTitle, UnderReview]]            
         df1 = df1.append(list1) #append list to dataframe, export to csv outside of the loop
@@ -422,16 +526,16 @@ print("Template auto-generation for highlights...\n")
 templateFilepath = '\\\\atlas\\lexispsl\\Highlights\\Automatic creation\\Templates\\All Highlights Template.xml'
 localDir = 'C:\\Users\\Hutchida\\Documents\\PSL\\Highlights\\xml\\Practice Areas\\'
 #outputDir = 'C:\\Users\\Hutchida\\Documents\\PSL\\Highlights\\xml\\Practice Areas\\'
-#outputDir = '\\\\atlas\\lexispsl\\Highlights\\Practice Areas\\'
-outputDir = '\\\\atlas\\lexispsl\\Highlights\\dev\\Practice Areas\\'
-reportDir = '\\\\atlas\\lexispsl\\Highlights\\dev\\Reports\\'
-#reportDir = '\\\\atlas\\lexispsl\\Highlights\\Automatic creation\\New and Updated content report\\'
+outputDir = '\\\\atlas\\lexispsl\\Highlights\\Practice Areas\\'
+#outputDir = '\\\\atlas\\lexispsl\\Highlights\\dev\\Practice Areas\\'
+#reportDir = '\\\\atlas\\lexispsl\\Highlights\\dev\\Reports\\'
+reportDir = '\\\\atlas\\lexispsl\\Highlights\\Automatic creation\\New and Updated content report\\'
 #reportDir = "C:\\Users\\Hutchida\\Documents\\PSL\\AICER\\reports\\"
 PublicLawFilepath = FindMostRecentFile(outputDir + 'Public Law\\', '*.xml')
 #BrexitTemplateFilepath = ('\\\\atlas\\lexispsl\\Highlights\\Practice Areas\\Brexit\\Brexit highlights template.xml')
 BrexitTemplateFilepath = FindMostRecentFile(outputDir + 'Brexit\\', '*.xml')
 lookupdpsi = '\\\\atlas\\knowhow\\PSL_Content_Management\\Digital Editors\\Lexis_Recommends\\lookupdpsi\\lookup-dpsis.csv'
-
+pguidlistDir = '\\\\lngoxfdatp16vb\\Fabrication\\MasterStore\\PGUID-Lists\\'
 
 print('Most recent Public Law highlight doc to harvest: \n' + PublicLawFilepath)
 print('Most recent Brexit template  to harvest: \n' + BrexitTemplateFilepath)
